3a09034b8cd66f579f89aa60861dcfa42330f8fa
Manoj G M <151445256+immanojgm@users.noreply.github.com>
2024-12-10 00:00:40 +0530

    cowsay(cowfile) changed

--- lion.sh
+++ lion.sh
@@ -3,10 +3,10 @@ sudo apt install cowsay -y
 
 
 #generating ASCII artwork
-cowsay -f lion "Run for me... I am a aggressive male lion" >> lion.txt
+cowsay -f lion "Run for me... I am a aggressive male lion" >> dragon.txt
 
 #Testing for availability of dragon.txt
-grep -i "lion" lion.txt
+grep -i "dragon" dragon.txt
 
 #Reading dargon.txt fie
-cat lion.txt
+cat dragon.txt
