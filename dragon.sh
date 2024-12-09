e34d2cded9d54069598ee49308e06d5498885583
Manoj G M <151445256+immanojgm@users.noreply.github.com>
2024-12-10 00:06:04 +0530

    errors resolved

--- dragon.sh
+++ dragon.sh
@@ -3,7 +3,7 @@ sudo apt install cowsay -y
 
 
 #generating ASCII artwork
-cowsay -f dragon "Run for me... I am a aggressive male lion" >> dragon.txt
+cowsay -f dragon "Run for me... I am a aggressive dragon with breathing fire" >> dragon.txt
 
 #Testing for availability of dragon.txt
 grep -i "dragon" dragon.txt
